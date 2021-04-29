59554017c81ef4e20b8d17dd344d5891339f2033
shan458 <77813639+shan458@users.noreply.github.com>
2021-04-29 14:44:13 -0700

    finished structured-english

---
+++ test_2.py
@@ -0,0 +1,11 @@
+"""
+The program will display the student grades, average grade, and the percentage of grades that are above the average grade. An input text file which contains the grades will be used in this program.  
+
+A main() function will be created to initialize the application. 
+
+Then a calculate_percent_above_average() function will be created to calculate the percentage of grades that are above the average grade. 
+
+The grades in the input text file will be read using the open() method and then the grades will be assigned to a list of integers.
+
+Lastly the information, which contains the student grades, average grade, and the percentage of grades that are above average, will be outputted to the user.
+"""
